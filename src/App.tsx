121e3789d50ec1d23ba37733c0a14aa8d8f719b6
user <mariiaantoniv@gmail.com>
2026-05-04 23:38:26 +0300

    <your commit message>

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,64 @@
-import React from 'react';
+import { Suspense } from 'react';
 import './App.scss';
+import { Footer } from './components/Footer';
+import { Header } from './components/Header';
+import { Loader } from './components/Loader';
+import { Outlet, Route, Routes } from 'react-router-dom';
+import { LikeProvider } from './context/LikeCard';
+import { CartProvider } from './context/CardContext';
+import { HomePage } from './pages/HomePage';
+import { ProductsPages } from './pages/ProductPages';
+import { ProductDetailsPage } from './pages/ProductDetailsPage';
+import { FavoritesPage } from './pages/FavoritesPage';
+import { CartPage } from './pages/CartPage';
+import { NoFoundPage } from './pages/NoFoundPage';
+import { AsidePage } from './pages/AsidePage/AsidePage';
 
-interface Props {
-  onClick: () => void;
-  children: React.ReactNode;
+function MainLayout() {
+  return (
+    <div className="layout">
+      <Header />
+      <main>
+        <div className="App" hidden>
+          <h1>Product Catalog</h1>
+        </div>
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </main>
+      <Footer />
+    </div>
+  );
 }
-
-export const Provider: React.FC<Props> = React.memo(({ onClick, children }) => (
-  <button type="button" onClick={onClick}>
-    {children}
-  </button>
-));
 
 export const App: React.FC = () => {
   return (
-    <div className="starter">
-      <Provider onClick={() => ({})}>TodoList</Provider>
-    </div>
+    <LikeProvider>
+      <CartProvider>
+        <Routes>
+          <Route element={<MainLayout />}>
+            <Route index element={<HomePage />} />
+            <Route path="aside" element={<AsidePage />} />
+            <Route
+              path="phones"
+              element={<ProductsPages category="phones" />}
+            />
+            <Route
+              path="tablets"
+              element={<ProductsPages category="tablets" />}
+            />
+            <Route
+              path="accessories"
+              element={<ProductsPages category="accessories" />}
+            />
+            <Route path="product/:productId" element={<ProductDetailsPage />} />
+
+            <Route path="favorites" element={<FavoritesPage />} />
+            <Route path="cart" element={<CartPage />} />
+            <Route path="*" element={<NoFoundPage />} />
+          </Route>
+        </Routes>
+      </CartProvider>
+    </LikeProvider>
   );
 };
